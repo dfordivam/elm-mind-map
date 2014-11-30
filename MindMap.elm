1cd0a2310eb4609ab3184d9febd6f6edcfcc6f63
Divam <dfordivam@gmail.com>
2014-11-30 04:01:18 -0500

    Fixed many bugs, now mimd-map functioning properly

--- MindMap.elm
+++ MindMap.elm
@@ -2,6 +2,7 @@ module MindMap where
 
 import Debug
 import Graphics.Input as Input
+import Maybe
 
 main : Signal Element
 main = lift view state 
@@ -48,7 +49,6 @@ data MM_Node = MM_Node
     ,  childNodes : [MM_Node]
     ,  text       : String
     ,  collapsed  : Bool
-    ,  parentNode : MM_Node
     ,  id         : Int 
     } | MM_RootNode
     { nodeName : String
@@ -65,13 +65,12 @@ type State =
     }
 
 -- Create a new node with given parent
-newMM_Node : MM_Node -> String -> Int-> MM_Node
-newMM_Node p val i = MM_Node
+newMM_Node : String -> Int-> MM_Node
+newMM_Node val i = MM_Node
     {  nodeName   = ""
     ,  childNodes = []
     ,  text       = val
     ,  collapsed  = False
-    ,  parentNode = p
     ,  id         = i
     }
 
@@ -80,32 +79,50 @@ emptyState =
     let root = MM_RootNode { nodeName = "", childNodes = [], text = "root", collapsed = False }
     in { rootNode = root, editNode = root , nodes = [root], uid = 0}
 
+getNodeID : MM_Node -> Int
+getNodeID n = 
+    case n of
+      (MM_Node node) -> node.id
+      (MM_RootNode node) -> 0
+
+getChildNodes : MM_Node -> [MM_Node]
+getChildNodes n = 
+    case n of
+      (MM_Node node) -> node.childNodes
+      (MM_RootNode node) -> node.childNodes
+-- 
 removeNodeWithId : Int -> [MM_Node] -> [MM_Node]
-removeNodeWithId id (n::ns) = 
-    let foundNode = 
-      case n of
-        (MM_Node node) -> node.id == id
-        (MM_RootNode node) -> True -- This cannot happen, root cannot be in childList
-    in if foundNode then ns else n :: (removeNodeWithId id ns)
- 
-getNodeWithId : Int -> [MM_Node] -> MM_Node
-getNodeWithId id (n::ns) = 
-    let foundNode = 
-      case n of
-        (MM_Node node) -> node.id == id
-        (MM_RootNode node) -> True -- This cannot happen, root cannot be in childList
-    in if foundNode then n else (getNodeWithId id ns)
+removeNodeWithId id list = 
+    if list == []
+    then [] 
+    else let n = head list
+             ns = tail list
+             foundNode =          
+              case n of
+                (MM_Node node) -> node.id == id
+                (MM_RootNode node) -> True
+         in if foundNode then ns else n :: (removeNodeWithId id ns)
+
+-- Linear search in list for node
+getNodeWithId : Int -> [MM_Node] -> Maybe MM_Node
+getNodeWithId id list = 
+    if list == [] 
+    then Nothing 
+    else let n = head list
+             ns = tail list
+         in if (id == getNodeID n) then Just n else (getNodeWithId id ns)
 
 -- How to add to tree
 -- 1. Create new empty node
--- 2. "add" this node to the target node (ie create a new node from target)
--- 3. update all the parent nodes (till root) with new nodes 
+-- 2. Get a list of all parent nodes till root
+-- 3. update all the parent nodes (starting from root) with new nodes 
 --    (ie create a new tree, with new root and new nodes)
-
-addNode : MM_Node -> String -> Int -> (MM_Node, MM_Node)
-addNode n val i = 
+-- root -> node -> val -> id -> (newRoot, newNode)
+addNode : MM_Node -> MM_Node -> String -> Int -> (MM_Node, MM_Node)
+addNode r n val i = 
     let 
-      newN = newMM_Node currentNode val i
+      newN = newMM_Node val i
+      parents = reverse (findAllParents (getNodeID n) r)
       currentNode = case n of
         (MM_Node node) ->
           let updatedChildList = newN :: node.childNodes
@@ -113,34 +130,52 @@ addNode n val i =
         (MM_RootNode node) ->
           let updatedChildList = newN :: node.childNodes
           in MM_RootNode { node | childNodes <- updatedChildList }
-    in (Debug.watch "up node" (updateNode currentNode), newN)
+    in ( (updateNode currentNode parents), newN)
+
+-- DFS for node
+dfsNode : Int -> MM_Node -> Bool
+dfsNode id r =
+      case r of
+        (MM_Node node) ->
+            if node.id == id
+            then True
+            else any (dfsNode id) node.childNodes 
+        (MM_RootNode _ ) -> True
+
+-- Get list of all parent nodes
+-- Node ID -> Root Node -> [Root :: All other parents]
+findAllParents : Int -> MM_Node -> [MM_Node]
+findAllParents id r = if (id == getNodeID r) then [] else
+    let foundNode = dfsNode id r
+    in if foundNode 
+       then r :: (concat (map (findAllParents id) (getChildNodes r)))
+       else []
 
 -- Takes an input "new" node (which is part of new tree)
 -- Create a new parent node by replacing old node with "new" node
 -- recursively call the update on parent node till root
 -- This API will return a new root node for new tree
-updateNode : MM_Node -> MM_Node
-updateNode newN =
-    case newN of
-      (MM_Node node) ->
-        let newParent = 
-          case (node.parentNode) of 
-            (MM_Node parent) ->
-              let updatedChildList = newN :: (removeNodeWithId node.id parent.childNodes)
-              in (MM_Node { parent | childNodes <- updatedChildList})
-            (MM_RootNode parent) ->
-              let updatedChildList = newN :: (removeNodeWithId node.id parent.childNodes)
-              in (MM_RootNode { parent | childNodes <- updatedChildList})            
-          
-        in updateNode newParent
-      (MM_RootNode node) ->
-        newN
+-- InputNode -> ParentNodes -> NewRoot
+updateNode : MM_Node -> [MM_Node] -> MM_Node
+updateNode newN list =
+    if list == [] then newN else
+        case newN of
+          (MM_Node node) ->
+            let newParent = 
+              case (head list) of 
+                (MM_Node parent) ->
+                  let updatedChildList = newN :: (removeNodeWithId node.id parent.childNodes)
+                  in (MM_Node { parent | childNodes <- updatedChildList})
+                (MM_RootNode parent) ->
+                  let updatedChildList = newN :: (removeNodeWithId node.id parent.childNodes)
+                  in (MM_RootNode { parent | childNodes <- updatedChildList})            
+              
+            in updateNode newParent (tail list)
+          (MM_RootNode node) ->
+            newN
 
 getAllNodes : MM_Node -> [MM_Node]
-getAllNodes n =
-    case n of
-      (MM_Node node) -> n :: (concat (map getAllNodes node.childNodes))
-      (MM_RootNode node) -> n :: (concat (map getAllNodes node.childNodes))
+getAllNodes n = n :: concat (map getAllNodes (getChildNodes n))
 
 testState : State
 testState = emptyState
@@ -188,9 +223,9 @@ step action state =
       NoOp -> state
       
       AddNode id ->
-        let node = getNodeWithId id state.nodes
-            updatedNodeList = Debug.watch "list" (getAllNodes updatedRoot)
-            (updatedRoot, newNode) = addNode node "child" (state.uid + 1)
+        let node = Maybe.maybe state.rootNode (\x -> x) (getNodeWithId id state.nodes)
+            updatedNodeList = (getAllNodes updatedRoot)
+            (updatedRoot, newNode) = addNode state.rootNode node "child" (state.uid + 1)
         in { state | uid <- (state.uid + 1), rootNode <- updatedRoot, editNode <- newNode, nodes <- updatedNodeList }
         
       
