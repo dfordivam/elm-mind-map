de271bd843ebf8da7008ad1cde758bbf4300e383
Divam <dfordivam@gmail.com>
2015-02-12 22:03:58 +0530

    Added html for tests

--- tests/Test.elm
+++ tests/Test.elm
@@ -2,11 +2,17 @@ module MindMap where
 import Graphics.Collage (..)
 import Color (..)
 
-main = 
-    let myForm1 = filled red (rect 40 20)
-        myForm2 = moveX 40 myForm1
-        myForm3 = filled green (rect 40 20)
-        myForm4 = group [moveY 30 myForm3, myForm3, moveY -30 myForm3]
-        myForm5 = moveX -40 myForm4
-        myForm  = group [myForm5, myForm2]
-    in collage 100 100 [myForm]
+-- Testing simple Collage APIs
+-- main = 
+--     let myForm1 = filled red (rect 40 20)
+--         myForm2 = moveX 40 myForm1
+--         myForm3 = filled green (rect 40 20)
+--         myForm4 = group [moveY 30 myForm3, myForm3, moveY -30 myForm3]
+--         myForm5 = moveX -40 myForm4
+--         myForm  = group [myForm5, myForm2]
+--     in collage 100 100 [myForm]
+
+import MM_StateMock (..)
+import RenderMap
+
+main = RenderMap.view (500, 500) twoNodes
